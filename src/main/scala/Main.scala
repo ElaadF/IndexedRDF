0fd73ed67c278edb8a8b64c24a0e20fc79ddbeeb
ElaadF <elaad.f@gmail.com>
2019-05-19 18:01:48 +0200

    Querie faster than IndexRDF ??

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,6 +4,8 @@ import edu.berkeley.cs.amplab.spark.indexedrdd.IndexedRDD
 import org.apache.spark.util.SizeEstimator
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.log4j.{Level, Logger}
+import org.apache.spark.rdd.RDD
+
 
 object Main extends App {
   val conf = new SparkConf().setAppName("simpleSparkApp").setMaster("local")
@@ -11,24 +13,28 @@ object Main extends App {
   val rootLogger = Logger.getRootLogger()
   rootLogger.setLevel(Level.OFF)
   val rdd = sc.textFile("resources/data/LiteMat/lubm1.ttl")
-  val str = "http://swat.cse.lehigh.edu/onto/univ-bench.owl#takesCourse"
+  val str = "<http://www.Department0.University0.edu/FullProfessor0><http://swat.cse.lehigh.edu/onto/univ-bench.owl#researchInterest>"
 
   val triple = Reader.extract(rdd)
   //triple.foreach(print(_))
 
-  val indexRDF = triple.map({case(s,p,o) => (s+p, o)})
+
+  val o_ps: RDD[(String, String)] = triple.map({case(s,p,o) => (o, p+s)})
+  val p_sp: RDD[(String, String)] = triple.map({case(s,p,o) => (p, s+p)})
+  val s_po: RDD[(String, String)] = triple.map({case(s,p,o) => (s, p+o)})
+  val sp_o: RDD[(String, String)] = triple.map({case(s,p,o) => (s+p, o)})
+  val op_s: RDD[(String, String)] = triple.map({case(s,p,o) => (o+p, s)})
+  val so_p: RDD[(String, String)] = triple.map({case(s,p,o) => (s+o, p)})
+  val spo: RDD[(String, String)] = triple.map({case(s,p,o) => (s+o+p, "None")})
+
+  val combinationToIndex = o_ps ++ p_sp ++ s_po ++ sp_o ++ op_s ++ so_p
   //indexRDF.foreach(x => println(x+"\n"))
 
-  val indexed = IndexedRDD(indexRDF).cache()
+  val indexed = IndexedRDD(combinationToIndex).cache()
 
   val classicSearch = Reader.time { Reader.search(triple, str) }
-  val indexedSearch = Reader.time { Reader.searchIndex(indexed, str) }
+  val indexedSearch = Reader.time { indexed.get(str) }
 
-
-  println(triple.first().toString())
-  println(indexed.first().toString())
-//  println(indexed.get(indexRDF.first()._1.toString))
-
-
+  println(indexed.get(str))
   println("\n\n\n Disk use : " + (SizeEstimator.estimate(indexed) / 1000000.0) + " Mb")
 }
--- src/main/scala/sparql/Queries.scala
+++ src/main/scala/sparql/Queries.scala
@@ -3,17 +3,34 @@ package sparql
 import java.io.FileInputStream
 import java.io.File
 
+import IO.Reader
 import org.apache.jena.rdf.model.ModelFactory
 import org.apache.jena.sparql.util.Utils
 import org.apache.jena.util._
+import org.apache.jena.query.Query
+import org.apache.jena.query.QueryExecution
+import org.apache.jena.query.QueryExecutionFactory
+import org.apache.jena.query.QueryFactory
+import org.apache.jena.query.ResultSetFormatter
 
 object Queries extends App {
-      val file = new File("resources/data/LiteMat/lubm1.ttl")
-      val input = new FileInputStream(file)
+  val file = new File("resources/data/LiteMat/lubm1.ttl")
+  val input = new FileInputStream(file)
+  val model = ModelFactory.createDefaultModel()
+  model.read(input, null, "TURTLE")
 
-      val model = ModelFactory.createDefaultModel()
-      model.read(input, null, "TURTLE")
+  val queryString = "" +
+    "PREFIX foaf: <http://xmlns.com/foaf/0.1/> " +
+    "SELECT ?o WHERE { <http://www.Department0.University0.edu/FullProfessor0> <http://swat.cse.lehigh.edu/onto/univ-bench.owl#researchInterest> ?o}"
 
-      System.out.println("\n---- Turtle ----")
-      model.write(System.out, "TURTLE")
+  val query = QueryFactory.create(queryString)
+
+  // Execute the query and obtain results
+  val qe = QueryExecutionFactory.create(query, model)
+  Reader.time { qe.execSelect }
+//  val results = qe.execSelect
+
+  // Output query results
+//  ResultSetFormatter.out(System.out, results, query)
+
 }
